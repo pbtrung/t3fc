b73cb527bbaaecbd1cb21389925716419249402a
Trung Pham <pham.bao.trung@gmail.com>
2019-02-06 22:14:12 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ ! -f build/release/sk ]; then
+if [ ! -f build/release/t3fc ]; then
     meson build/release --buildtype release
 fi
 ninja -C build/release
 
-if [ ! -f build/debug/sk ]; then
+if [ ! -f build/debug/t3fc ]; then
     meson build/debug --buildtype debug
 fi
 ninja -C build/debug
